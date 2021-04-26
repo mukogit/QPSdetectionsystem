52aa132e22eec375652da5b3cc13324175bc568d
Korbinian Muehlberger <km@lasers.kth.se>
2021-04-26 12:22:55 +0200

    update

--- software/python/main.py
+++ software/python/main.py
@@ -1,16 +1,28 @@
-
 import serial
+import os
+import datetime
 import numpy as np
 
+port = 'COM1'
+
 teensy, data = None, None
-port, baud = 'COM1', 2000000
+baud = 2000000
 voltage1_offset = 900 #measurement dep. value
 voltage2_offset = 600 #measurement dep. value
 pc = 0 				  #2@$\pi$@ phasecounter
-phase_off = 0 		  #phase offset    
-pidiff = 5
+phase_offset = 0 		  #phase offset    
+pidiff = 6
 
 teensy = serial.Serial(port, baudrate=baud)
+
+directory = os.getcwd()
+now = datetime.datetime.now()
+filename = now.strftime('%y%m%d_%H%M%S')
+
+path = ('{0}\\data\\{1}_data.csv'.format(directory, filename))
+outfile = open(path, 'w+')
+outfile.write('time,v1,v2,unwrapped phase,position\n')
+
 data_acquisition = True
 
 while data_acquisition:
@@ -31,13 +43,14 @@ while data_acquisition:
     
     #unwrap phase
     wrap_phase = np.arctan2(v2_norm, v1_norm)
-    delta_wrap_phase = wrap_phase - phase_off
+    delta_wrap_phase = wrap_phase - phase_offset
     if delta_wrap_phase > pidiff:
         pc -= 1
     elif delta_wrap_phase < -pidiff:
         pc += 1
-    phase_off = wrap_phase
+    phase_offset = wrap_phase
     phase = wrap_phase + (2*np.pi * pc)
     
     #convert phase to position
     position = phase / (4*np.pi) * 632.8e-9 #[m]
+    outfile.write('{0},{1},{2},{3:.4f},{4:.4f}\n'.format(time, v1, v2, phase, position))
